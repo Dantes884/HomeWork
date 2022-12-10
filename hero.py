4984a0b1d7fe41f27300a06fb3445c62b72c5953
Dantes884 <dzen.99@mail.ru>
2022-12-10 12:42:16 +0600

    second hw in first hw

--- hero.py
+++ hero.py
@@ -20,10 +20,54 @@ class SuperHero:
         return len(self.catchphrase)
 
 
-
-
 hero = SuperHero('Shen', 'The eye of Twilight', 'Twilight sword', 610, 'Demonstration of superior jugdment.')
 hero.run()
 hero.hp()
 print(hero)
 print(len(hero))
+
+class TreeHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+
+    def hp(self):
+        self.health_points **= 2
+        self.fly = True
+    def fly_phrase(self):
+        print('fly in the True_phrase')
+
+tree = TreeHero('Maokai', 'The Twisted Treant', "Nature's Grasp", 635, 'The islands will bloom again.')
+tree.run()
+tree.hp()
+print(tree)
+print(len(tree))
+tree.fly_phrase()
+class EarthHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+
+    def hp(self):
+        self.health_points **= 2
+        self.fly = True
+    def fly_phrase(self):
+        print(f'fly in the {self.fly}_phrase')
+
+earth = EarthHero('Sett', 'The Boss', 'Haymaker', 670, "I'm undisputed.")
+earth.run()
+earth.hp()
+print(earth)
+print(len(earth))
+earth.fly_phrase()
+
+class Villian(EarthHero):
+    people = 'monster'
+    def gen_x(self):...
+    def crit(self):
+        print(f'crit damage {self ** 3}')
+
+drake = Villian('Dragon', 'Cloud drake', 'Cloud', 5730, 'I am the dragon!')
+Villian.crit(earth.damage)
